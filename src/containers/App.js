383a48aa91c5042601cb3df5df97347cccda4e3a
ehlinashin <ehlinashin@yandex.ru>
2019-11-17 17:30:52 +0300

    remove unused code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import MainCityContainer from "./MainCityContainer";
 import CityListContainer from "./CityListContainer";
-// import './App.css';
 
 class App extends React.Component {
   render() {
